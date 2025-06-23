69f4d001acdc25f0ec045d60db45c7ad08784cc9
rahulkumarclg <rahul.code2024@gmail.com>
2025-06-23 23:59:52 +0530

    area of circle

--- CompetitiveCode/02_areaOfCircle.cpp
+++ CompetitiveCode/02_areaOfCircle.cpp
@@ -1,12 +1,17 @@
 #include<iostream>
+#include<iomanip>  // Required for setprecision decimal point.
+#include<math.h>
 #define PI 3.14159265   //==== It will be fixed value.
 
 using namespace std;
 float FindAreaOfCircle(float r){
-    return PI *(r * r);
+
+    // return PI *(r * r);
+    return PI * pow(r, 2);
 };
 
 int main(){
+    cout<< fixed << setprecision(2);
     cout<< FindAreaOfCircle(10);
     return 0;
 };
